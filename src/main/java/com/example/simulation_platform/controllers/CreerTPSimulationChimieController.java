66ac96828eccc8e39d88d9a850864f3e783f8b47
iamsouane <ismailasouane08@gmail.com>
2025-01-09 13:24:24 +0000

    vf ajout texte sous les bechers dans simulation chimie

--- src/main/java/com/example/simulation_platform/controllers/CreerTPSimulationChimieController.java
+++ src/main/java/com/example/simulation_platform/controllers/CreerTPSimulationChimieController.java
@@ -58,11 +58,15 @@ public class CreerTPSimulationChimieController {
     }
 
     private void create3DSimulation() {
-        // Créer un bécher 1 avec étiquette
-        Group becher1Group = createBecherWithLabel(-50, "Bécher 1");
+        // Créer un bécher 1
+        Cylinder becher1 = createBecher(-50);
 
-        // Créer un bécher 2 avec étiquette
-        Group becher2Group = createBecherWithLabel(50, "Bécher 2");
+        // Créer un bécher 2
+        Cylinder becher2 = createBecher(50);
+
+        // Créer les étiquettes fixes sous chaque bécher
+        Text becher1Label = createFixedLabel(-30, 50, "Bécher 1");
+        Text becher2Label = createFixedLabel(70, 50, "Bécher 2");
 
         // Créer le fond de la simulation (représenté par un rectangle)
         Rectangle fond = new Rectangle(400, 400);
@@ -70,30 +74,27 @@ public class CreerTPSimulationChimieController {
         fond.setTranslateZ(-1); // Assurez-vous que le fond est derrière les béchers
 
         // Ajouter les objets à la scène 3D
-        simulationContainer.getChildren().addAll(fond, becher1Group, becher2Group);
+        simulationContainer.getChildren().addAll(fond, becher1, becher2, becher1Label, becher2Label);
 
         // Déplacer le bécher 1 vers le bécher 2 pour simuler l'action de verser en arc de cercle
-        moveBecherToOther((Cylinder) becher1Group.getChildren().get(0), (Cylinder) becher2Group.getChildren().get(0));
+        moveBecherToOther(becher1, becher2);
     }
 
-    private Group createBecherWithLabel(double positionX, String label) {
-        // Créer un cylindre pour représenter le bécher
+    private Text createFixedLabel(double positionX, double offsetY, String label) {
+        Text textLabel = new Text(label);
+        textLabel.setFill(Color.BLACK);
+        textLabel.setTranslateX(positionX - 15);  // Centrer légèrement par rapport au bécher
+        textLabel.setTranslateY(50 + offsetY);  // Position sous le bécher (en fonction de la hauteur du bécher)
+        return textLabel;
+    }
+
+    private Cylinder createBecher(double positionX) {
         Cylinder becher = new Cylinder(20, 100);  // Rayon et hauteur
         PhongMaterial material = new PhongMaterial();
         material.setDiffuseColor(Color.rgb(192, 192, 192, 0.2));  // Couleur argentée avec transparence
         becher.setMaterial(material);
         becher.setTranslateX(positionX);
-
-        // Ajouter un texte sous le bécher
-        Text becherLabel = new Text(label);
-        becherLabel.setFill(Color.BLACK);
-        becherLabel.setTranslateY(70); // Positionner sous le bécher
-        becherLabel.setTranslateX(positionX - 15);
-
-        // Regrouper le bécher et son texte
-        Group becherGroup = new Group(becher, becherLabel);
-        becherGroup.setTranslateX(positionX);
-        return becherGroup;
+        return becher;
     }
 
     private void moveBecherToOther(Cylinder becher1, Cylinder becher2) {
@@ -102,12 +103,12 @@ public class CreerTPSimulationChimieController {
         path.getElements().add(new MoveTo(becher1.getTranslateX(), becher1.getTranslateY()));
 
         // Crée un arc de cercle qui finit à côté de becher2
-        double offsetX = -5;  // Décalage horizontal pour être à côté de becher2
+        double offsetX = -50;  // Décalage horizontal pour être à côté de becher2
         path.getElements().add(new QuadCurveTo(
                 (becher1.getTranslateX() + becher2.getTranslateX()) / 2, // Point de contrôle pour l'arc
                 becher1.getTranslateY() - 150,                          // Hauteur maximale de l'arc
                 becher2.getTranslateX() + offsetX,                      // Position finale : à côté de becher2
-                becher2.getTranslateY() - 120                           // Position finale : au niveau de becher2
+                becher2.getTranslateY() - 90                          // Position finale : au niveau de becher2
         ));
 
         PathTransition pathTransition1 = new PathTransition();
